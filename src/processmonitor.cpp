87506577eb3104f8c7a022f15be521f99ca09f4b
Artem S. Tashkinov <birdie-github@users.noreply.github.com>
2026-07-02 14:52:05 +0400

    A possible Windows build fix

--- src/processmonitor.cpp
+++ src/processmonitor.cpp
@@ -13,6 +13,12 @@
 #include <limits>
 
 #ifdef Q_OS_WIN
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #include <tlhelp32.h>
 #else
